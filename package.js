304ae9349680b7028af7cb7a880b362fbd77e9cf
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2019-06-13 22:41:33 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,34 @@
+{
+  "name": "assignment5",
+  "version": "1.0.0",
+  "description": "ITC230 templating with handlebars",
+  "main": "index.js",
+  "directories": {
+    "test": "test"
+  },
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/Espoir333/ITC230-Assignment5/new/master"
+  },
+  "keywords": [
+    "\"database\"",
+    "\"Espoir",
+    "N",
+    "Makiese\"",
+    "\"ITC230\"",
+    "\"Seattle",
+    "Central",
+    "College\""
+  ],
+  "author": "Espoir N Makiese",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.17.1",
+    "express": "^4.15.2",
+    "express-handlebars": "^3.0.0",
+    "mongoose": "^3.8.6"
+  }
+}
